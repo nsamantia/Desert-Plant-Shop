7ffb81b173ea4e9aea13b39970bc4486cafa3699
namantia1@gmail.com <namantia1@gmail.com>
2020-09-24 17:04:40 -0400

    first commit - sever online

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./authController')
+const plntCtrl = require('./controller')
+// const verifyUser = require('./middlewares/verifyUser')
+
+const app = express()
+
+const{CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
+
+app.use(express.json())
+app.use(
+    session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
+    })
+)
+
+//Auth endpoints
+
+
+//plants endpoints
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+    app.set('db', dbInstance)
+    console.log('DB ready!')
+    app.listen(SERVER_PORT, () =>
+    console.log(`Server ready on port ${SERVER_PORT}`))
+})
