cd106cbe16421be33a7f7d15535253a470e347e1
namantia1@gmail.com <namantia1@gmail.com>
2021-05-10 17:02:11 -0400

    succs.js converted using react hooks

--- src/Components/Succs/Succs.js
+++ src/Components/Succs/Succs.js
@@ -1,55 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Succs.scss';
 import axios from 'axios';
 import SuccsList from './SuccsList';
 import Nav from '../Nav/Nav';
 
-class Succs extends Component {
-  constructor() {
-    super();
-    this.state = {
-      succs: [],
-    };
-  }
+const Succs = () => {
+  const [succs, setSuccs] = useState([]);
 
-  componentDidMount() {
-    this.getSuccs();
-  }
+  useEffect(() => {
+    getSuccs();
+  }, []);
 
-  getSuccs = () => {
-    axios
-      .get(`/api/succs`)
-      .then((res) => {
-        this.setState({
-          succs: res.data,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const getSuccs = () => {
+    axios.get(`/api/succs`).then((res) => setSuccs(res.data));
   };
 
-  render() {
-    const mappedSuccs = this.state.succs.map((element) => {
-      return (
-        <SuccsList
-          succsListing={element}
-          key={element.id}
-          name={element.name}
-          price={element.price}
-          image={element.img}
-        />
-      );
-    });
-
+  const mappedSuccs = succs.map((element) => {
     return (
-      <div>
-        <Nav />
-        <h2>Succulents</h2>
-        <div className="mapped-succs-container">{mappedSuccs}</div>
-      </div>
+      <SuccsList
+        succsListing={element}
+        key={element.id}
+        name={element.name}
+        price={element.price}
+        image={element.img}
+      />
     );
-  }
-}
+  });
+  return (
+    <div>
+      <Nav />
+      <h2>Succulents</h2>
+      <div className="mapped-succs-container">{mappedSuccs}</div>
+    </div>
+  );
+};
+// class Succs extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       succs: [],
+//     };
+//   }
+
+//   componentDidMount() {
+//     this.getSuccs();
+//   }
+
+//   getSuccs = () => {
+//     axios
+//       .get(`/api/succs`)
+//       .then((res) => {
+//         this.setState({
+//           succs: res.data,
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+
+//   render() {
+//     const mappedSuccs = this.state.succs.map((element) => {
+//       return (
+//         <SuccsList
+//           succsListing={element}
+//           key={element.id}
+//           name={element.name}
+//           price={element.price}
+//           image={element.img}
+//         />
+//       );
+//     });
+
+//     return (
+//       <div>
+//         <Nav />
+//         <h2>Succulents</h2>
+//         <div className="mapped-succs-container">{mappedSuccs}</div>
+//       </div>
+//     );
+//   }
+// }
 
 export default Succs;
